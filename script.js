64004ef68da24ad3a559fd0284787889b42191e0
uzmamstf <49521264+uzmamstf@users.noreply.github.com>
2019-06-13 16:08:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+let distance = 12;
+const distanceBetweenSunAndEarth = 1000000;
+
+
+/*  Data = Information
+
+Data Type
+-number like (1234...)
+-string (alphabets or letters) like ( abc...)
+-boolean ( true or false )
+-undefined/ null ( pta ni )
+
+*/
+//number
+//1. Distance is 10 km
+distance = 10;
+//2. number of students in cl-4 is 50
+let students = 50;
+
+
+//string
+// My name is Reena
+let name= "Reena";
+// qoutation mark bt both single or double ''  ""
+
+// methods
+//1. "Reena"
+//2.'Reena'
+//2 `Reena` (backticks) best way to write
+
+//other example   
+// this program name is Tech karo
+let program = "Tech karo";
+
+//boolean type
+//1. kal barish nahi hogi
+let isRaining = false;
+// it always write is or has
+// 2. kya practice ho rhi hai? haan
+let isPractice = true;
+
+// undefined example
+//1.kya pakistan worldvoup2019 jeetyga
+// let iswining;
+// let iswining = null;
+//et iswi lning = undefined;
+
+function signUp(){
+
+let isAgreed = document.querySelector('#terms').checked;
+
+if(isAgreed == true){
+
+    alert("Congratulations ");
+}
+else{
+
+    alert("Error, Please agreed to terms");
+}
+}
+
+function login(){
+
+    
+}
